ee89aaeae0b1ddf03ebe9ec1611de8ee7b573972
David Chanin <chanindav@gmail.com>
2020-11-30 21:20:32 +0000

    trying updated dockerfile

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,6 @@
+import os
 from app import app
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=int(os.environ.get('PORT', 5000)))
