dac988f3a13366ffbfe66e738b3de73d9e99ade3
David Chanin <chanindav@gmail.com>
2020-12-07 23:22:55 +0000

    updating root path

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import "./App.css";
-import Intro from "./Intro";
-import Query from "./Query";
+import './App.css';
+import Intro from './Intro';
+import Query from './Query';
+
+const ROOT_PATH = process.env.REACT_APP_ROOT_PATH || '/';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={ROOT_PATH}>
         <Switch>
           <Route exact path="/">
             <Intro />
