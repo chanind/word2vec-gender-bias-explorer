dbb5cd71f733d8f6cda81ddf479e7c389db0e7b2
David Chanin <chanindav@gmail.com>
2020-12-09 23:44:38 +0000

    switch to hash router

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Intro from './Intro';
 import Query from './Query';
 import OctocatCorner from './components/OctocatCorner';
 
-const ROOT_PATH = process.env.REACT_APP_ROOT_PATH || '/';
-
 function App() {
   return (
     <div className="App">
       <OctocatCorner />
-      <Router basename={ROOT_PATH}>
+      <Router>
         <Switch>
           <Route exact path="/">
             <Intro />
