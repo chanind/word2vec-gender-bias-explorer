82c057a8b9cefdc9a1c2ef962e8daf4cb08eb4b8
David Chanin <chanindav@gmail.com>
2020-12-21 23:39:50 +0000

    experimenting with PCA

--- server/app/main.py
+++ server/app/main.py
@@ -3,4 +3,4 @@ from app import app
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", debug=True, port=int(os.environ.get("PORT", 5000)))
