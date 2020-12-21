8a8e88206e63f5e911c38d41a77bf25687da7b0c
David Chanin <chanindav@gmail.com>
2020-12-21 23:42:17 +0000

    using an averaged bias vector instead of just he/she

--- server/app/detect_bias.py
+++ server/app/detect_bias.py
@@ -1,13 +1,23 @@
 from model import model
 
-biased_pairs = [
-    (model["he"], model["she"]),
+biased_word_pairs = [
+    ("she", "he"),
+    ("her", "his"),
+    ("woman", "man"),
+    ("Mary", "John"),
+    ("herself", "himself"),
+    ("daughter", "son"),
+    ("mother", "father"),
+    ("gal", "guy"),
+    ("girl", "boy"),
 ]
+
+biased_pairs = [(model[pair[0]], model[pair[1]]) for pair in biased_word_pairs]
 
 origins = [(pair[0] + pair[1]) / 2 for pair in biased_pairs]
 origin = sum(origins) / len(origins)
 
-# simple detection, just use basic subtraction to get gender vector
+# simple detection, just use basic subtraction and averaging to get gender vector
 
 biases = [pair[0] - pair[1] for pair in biased_pairs]
 bias = sum(biases) / len(biases)
@@ -16,4 +26,4 @@ bias = sum(biases) / len(biases)
 def detect_bias(word):
     if word not in model:
         return None
-    return model.cosine_similarities(bias, [model[word] - origin])[0].astype(float)
+    return -1 * model.cosine_similarities(bias, [model[word] - origin])[0].astype(float)
