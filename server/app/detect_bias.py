98278415ac16778db9bf6ae809dbb02d5478a95e
David Chanin <chanindav@gmail.com>
2020-12-23 22:40:21 +0000

    fixing up PCA by making sure the difference vectors are centered properly

--- server/app/detect_bias.py
+++ server/app/detect_bias.py
@@ -15,49 +15,38 @@ biased_word_pairs = [
 ]
 
 biased_pairs = [(model[pair[0]], model[pair[1]]) for pair in biased_word_pairs]
-
-origins = [(pair[0] + pair[1]) / 2 for pair in biased_pairs]
-origin = sum(origins) / len(origins)
+biases = [pair[0] - pair[1] for pair in biased_pairs]
+reversed_biases = [pair[1] - pair[0] for pair in biased_pairs]
 
 # simple detection, just use basic subtraction to get gender vector
 
-biases = [pair[0] - pair[1] for pair in biased_pairs]
-all_gendered_vects = []
-for pair in biased_pairs:
-    all_gendered_vects.append(pair[0])
-    all_gendered_vects.append(pair[1])
-
 bias = sum(biases) / len(biases)
-
-pca = PCA(n_components=1)
-pca.fit(np.array(biases))
-
-female_mean = np.mean(pca.transform(np.array([pair[0] for pair in biased_pairs])))
-male_mean = np.mean(pca.transform(np.array([pair[1] for pair in biased_pairs])))
+origins = [(pair[0] + pair[1]) / 2 for pair in biased_pairs]
+origin = sum(origins) / len(origins)
 
 
-def detect_bias(word):
+def detect_bias_simple(word):
+    """
+    Simple bias algorithm, just average all the bias vectors together and use cosine similarity
+    """
     if word not in model:
         return None
     return -1 * model.cosine_similarities(bias, [model[word] - origin])[0].astype(float)
 
 
+pca = PCA(n_components=1)
+pca.fit(np.array(biases + reversed_biases))
+
+female_mean = np.mean(pca.transform(np.array([pair[0] for pair in biased_pairs])))
+male_mean = np.mean(pca.transform(np.array([pair[1] for pair in biased_pairs])))
+
+
 def detect_bias_pca(word):
+    """
+    Use PCA to find the gender bias vector, and determine bias based on position along the gender vector
+    """
     if word not in model:
         return None
     word_val = pca.transform(np.array([model[word]]))[0][0]
     # rescaling word value so that the male/female average maps to 1 and -1
     return (word_val - (male_mean + female_mean) / 2) * 2 / (male_mean - female_mean)
-
-
-print("PCA training complete! result:")
-print(pca.explained_variance_ratio_)
-print("Female bias scores, non-pca")
-print([detect_bias(p[0]) for p in biased_word_pairs])
-print("Male bias scores, non-pca")
-print([detect_bias(p[1]) for p in biased_word_pairs])
-print("PCA")
-print("Female bias scores, pca")
-print([detect_bias_pca(p[0]) for p in biased_word_pairs])
-print("Male bias scores, pca")
-print([detect_bias_pca(p[1]) for p in biased_word_pairs])
